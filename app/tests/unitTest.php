0818fcadf1dfaef55691914ad70880bf01bc19bd
ParkerKaiMalachai <irinkademchuuuuuuuk@gmail.com>
2024-11-14 11:46:42 +0300

    code fixes

--- app/tests/unitTest.php
+++ app/tests/unitTest.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 require 'app/autoload.php';
 
 use PHPUnit\Framework\TestCase;
+use src\classes\Product;
 
 class unitTest extends TestCase
 {
@@ -16,8 +17,9 @@ class unitTest extends TestCase
     {
         $mock = Mockery::mock('Product');
         $mock->shouldReceive('getProfit')->andReturn(50);
+        $product = new Product($mock);
 
-        $this->assertSame(50, $mock->getProfit(5, 10));
+        $this->assertSame(50, $product->getProfit(5, 10));
     }
 
     /*
@@ -27,8 +29,9 @@ class unitTest extends TestCase
     public function testGetDecrease(): void
     {
         $mock = Mockery::mock('Product');
-        $mock->shouldReceive('getDecrease')->andReturn((18));
+        $mock->shouldReceive('getDecrease')->andReturn(18);
+        $product = new Product($mock);
 
-        $this->assertSame(18, $mock->getDecrease(3, 6));
+        $this->assertSame(18, $product->getDecrease(3, 6));
     }
 }
